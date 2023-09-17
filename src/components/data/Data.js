90933bda69095c775e8b6cef7025233e92e89c2d
Anshuman Parmar <anshumansp16@gmail.com>
2023-09-17 16:45:37 +0530

    added featured and recent component

--- src/components/data/Data.js
+++ src/components/data/Data.js
@@ -1,3 +1,16 @@
+import h1 from '../../assets/images/hero/h1.png'
+import h2 from '../../assets/images/hero/h2.png'
+import h3 from '../../assets/images/hero/h3.png'
+import h4 from '../../assets/images/hero/h4.png'
+import h6 from '../../assets/images/hero/h6.png'
+
+import p1 from '../../assets/images/list/p-1.png'
+import p2 from '../../assets/images/list/p-2.png'
+import p4 from '../../assets/images/list/p-4.png'
+import p5 from '../../assets/images/list/p-5.png'
+import p6 from '../../assets/images/list/p-6.png'
+import p7 from '../../assets/images/list/p-7.png'
+
 export const nav  = [
     {
         text: "home",
@@ -25,9 +38,90 @@ export const nav  = [
     },
 ]
 
-export const featured  = []
+export const featured  = [
+    {
+        name: "Family House",
+        total: "122 Property",
+        cover: h1
+    },
+    {
+        name: "House & Villa",
+        total: "155 Property",
+        cover: h2
+    },
+    {
+        name: "Apartment",
+        total: "300 Property",
+        cover: h3
+    },
+    {
+        name: "Office & Studio",
+        total: "80 Property",
+        cover: h4
+    },
+    {
+        name: "Villa & Condo",
+        total: "80 Property",
+        cover: h6
+    }
+]
 
-export const list  = []
+export const list  = [
+    {
+        id: 1,
+        cover: p1 ,
+        name: "Red Carpet Real Estate",
+        location: "210 Zirak Road, Canada",
+        category: "For Rent",
+        price: "$3,700",
+        type: "Apartment",
+      },
+      {
+        id: 2,
+        cover: p2,
+        name: "Fairmount Properties",
+        location: "5698 Zirak Road, NewYork",
+        category: "For Sale",
+        price: "$9,750",
+        type: "Condos",
+      },
+      {
+        id: 3,
+        cover: p4,
+        name: "The Real Estate Corner",
+        location: "5624 Mooker Market, USA",
+        category: "For Rent",
+        price: "$5,860",
+        type: "Offices",
+      },
+      {
+        id: 4,
+        cover: p5,
+        name: "Herringbone Realty",
+        location: "5621 Liverpool, London",
+        category: "For Sale",
+        price: "$7,540",
+        type: "Homes & Villas",
+      },
+      {
+        id: 5,
+        cover: p6,
+        name: "Brick Lane Realty",
+        location: "210 Montreal Road, Canada",
+        category: "For Rent",
+        price: "$4,850",
+        type: "Commercial",
+      },
+      {
+        id: 6,
+        cover: p7,
+        name: "Banyon Tree Realty",
+        location: "210 Zirak Road, Canada",
+        category: "For Sale",
+        price: "$2,742",
+        type: "Apartment",
+      }
+]
 
 export const awards  = []
 
---
+++ src/components/home/recent/Recent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Heading from '../../common/Heading'
+import RecentCard from './RecentCard'
+
+const Recent = () => {
+  return (
+    <>
+      <section className='recent padding'>
+        <div className='container'>
+            <Heading title="Recent Property Listed" subtitle="Explore the latest additions to our property listings, featuring a diverse range of homes, apartments, and investment opportunities." />
+            <RecentCard />
+        </div>
+      </section>
+    </>
+  ) 
+}
+
+export default Recent
